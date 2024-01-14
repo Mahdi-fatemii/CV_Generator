9f47a05376d1232ea2131d49f6ea497957ee37b2
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2024-01-14 20:30:17 +0330

    finishing project for now

--- pdf/views.py
+++ pdf/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Profile
 from .forms import *
+import pdfkit
+from django.http import HttpResponse
+from django.template import loader
+import io, os
 # Create your views here.
 
 
@@ -17,4 +21,21 @@ def accept(request):
 
 def resume(request, id):
     user_form = Profile.objects.get(pk=id)
-    return render(request, 'pdf/resume.html', {'user_form': user_form})
+    template = loader.get_template('pdf/resume.html')
+    html = template.render({'user_form': user_form})
+    options = {
+        'page-size': 'Letter',
+        'encoding': 'UTF-8',
+    }
+    config = pdfkit.configuration(wkhtmltopdf='C:\\Users\\MT1ShotYT\\Desktop\\CV_Generator\\wkhtmltox\\bin\\wkhtmltopdf.exe')
+    pdf = pdfkit.from_string(html, False, options,configuration=config)
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment'
+    filename = "resume.pdf"
+
+    return response
+
+
+def user_lists(request):
+    profiles = Profile.objects.all()
+    return render(request, 'pdf/list.html', {'profiles': profiles})
